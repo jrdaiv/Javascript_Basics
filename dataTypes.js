14794ffe7e64091221dde48c0120bcdc5304bdb0
jrdaiv <jrdaiv@gmail.com>
2024-07-22 17:47:50 -0700

    Javascript Basics and Syntax

---
+++ dataTypes.js
@@ -0,0 +1,25 @@
+// Task 1, 2, and 3
+
+let personName = "Bob"; //name
+let numAge = 30; //age
+let numGrade = "12th"; //grade
+let attendanceStatus = "Present"// status
+
+
+
+console.log("Student Name: " + personName)
+console.log("Student Age: " + numAge) 
+console.log("Student Grade: " + numGrade)
+console.log("Attendance Status: " + attendanceStatus)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javaScriptOperators.js
@@ -0,0 +1,45 @@
+// Task 1, and 2 //
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+let num3 = 20;
+let num4 = 10;
+let sum2 = num3 - num4;
+let num5 = 15;
+let num6 = 30;
+let sum3 = num5 * num6;
+let num7 = 30;
+let num8 = 15;
+let sum4 = num7 / num8;
+
+// Task 3 //
+console.log("Sum:", sum);
+console.log("Difference:", sum2);
+console.log("Product:", sum3);
+console.log("Quotent:", sum4);
+
+
+// task 4 //
+num1 += 5;
+num2 -= 10;
+
+console.log("Updated num1:", num1);
+console.log("Updated num2:", num2);
+
+// Task 5 //
+num1 = 20;
+num2 = 50;
+
+console.log("Is num1 equal to num2?", num1 == num2);
+console.log("Is num1 greater than num2?",num1 > num2 );
+console.log("Is num1 not equal to num2?",num1 != num2 );
+
+// Task 6 //
+let isPositive = num1 > 0 && num2 > 0;
+let isEven = num1 & 2 == 0 || num2 & 2 == 0;
+
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
+
+
+
